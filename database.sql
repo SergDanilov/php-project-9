96747bbd22e55ce991d9df9de33b60155b59a381
Sergey Dan <danilovserg1985s@gmail.com>
2024-11-03 14:45:22 +0000

    added url_checks table

--- database.sql
+++ database.sql
@@ -7,4 +7,15 @@ ALTER TABLE urls
 ADD COLUMN response_code INT,
 ADD COLUMN h1 VARCHAR(255),
 ADD COLUMN title VARCHAR(255),
-ADD COLUMN description TEXT;
+ADD COLUMN description TEXT,
+ADD COLUMN last_check TIMESTAMP;
+
+CREATE TABLE url_checks (
+    id SERIAL PRIMARY KEY,
+    url_id SERIAL REFERENCES urls(id) NOT NULL, 
+    status_code INT,
+    h1 VARCHAR(255),
+    title VARCHAR(255),
+    description TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP()  
+);
