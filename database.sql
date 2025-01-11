ebed4b25ce257e106513fcabf7c67806df558dd7
Sergey Dan <danilovserg1985s@gmail.com>
2025-01-11 14:51:37 +0000

    fixed and returned database.sql with tables

---
+++ database.sql
@@ -0,0 +1,25 @@
+-- Создание таблицы urls
+CREATE TABLE urls (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Добавление столбцов в таблицу urls
+ALTER TABLE urls
+    ADD COLUMN response_code INT,
+    ADD COLUMN h1 VARCHAR(255),
+    ADD COLUMN title VARCHAR(255),
+    ADD COLUMN description TEXT,
+    ADD COLUMN last_check TIMESTAMP;
+
+-- Создание таблицы url_checks
+CREATE TABLE url_checks (
+    id SERIAL PRIMARY KEY,
+    url_id INTEGER REFERENCES urls(id) ON DELETE CASCADE, 
+    status_code INT,
+    h1 VARCHAR(255),
+    title VARCHAR(255),
+    description TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
