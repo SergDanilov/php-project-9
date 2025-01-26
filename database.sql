46c563b1b5a27c9ef81ebcad0207e5108451df30
Sergey Dan <danilovserg1985s@gmail.com>
2025-01-26 16:12:06 +0000

    fixed urls table

--- database.sql
+++ database.sql
@@ -1,21 +1,17 @@
 -- Создание таблицы urls
 GRANT ALL PRIVILEGES ON TABLE urls TO analyzer_user;
 DROP TABLE IF EXISTS urls;
+
 CREATE TABLE urls (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    response_code INT,
+    h1 VARCHAR(255),
+    title VARCHAR(255),
+    description TEXT,
+    last_check TIMESTAMP
 );
-
--- Добавление столбцов в таблицу urls
-ALTER TABLE urls
-    ADD COLUMN response_code INT,
-    ADD COLUMN h1 VARCHAR(255),
-    ADD COLUMN title VARCHAR(255),
-    ADD COLUMN description TEXT,
-    ADD COLUMN last_check TIMESTAMP;
-
-
 
 -- Создание таблицы url_checks
 DROP TABLE IF EXISTS url_checks;
