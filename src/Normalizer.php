d83741c14dd152454233ff2bb559af9a63cf0182
Sergey Dan <danilovserg1985s@gmail.com>
2025-03-03 11:21:01 +0000

    ref linter

--- src/Normalizer.php
+++ src/Normalizer.php
@@ -2,17 +2,27 @@
 
 namespace App;
 
+use Illuminate\Support\Arr;
+
 class Normalizer
 {
-    public function normalizeUrl(string $url): ?string
+    public function normalizeUrl(string $url): string|null
     {
         $url = mb_strtolower(trim($url));
+
+        // Добавляем схему, если её нет
         if (!parse_url($url, PHP_URL_SCHEME)) {
-                $url = "http://{$url}";
+            $url = "http://{$url}";
         }
+
         $parts = parse_url($url);
-        return ($parts && !empty($parts['host']))
-                ? "{$parts['scheme']}://{$parts['host']}"
-                : null;
+
+        if (!$parts || empty($parts['host'])) {
+            return null;
+        }
+        // По умолчанию 'http', если схема не указана
+        $scheme = Arr::get($parts, 'scheme', 'http');
+
+        return "{$scheme}://{$parts['host']}";
     }
 }
